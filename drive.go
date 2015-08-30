1dd4d70d5ac4b35b191b503172494733d63e8c2b
Claire Blackshaw <Kimau@users.noreply.github.com>
2015-08-30 11:38:21 +0100

    Update drive.go

--- drive.go
+++ drive.go
@@ -10,41 +10,32 @@ import (
 	urlshortener "google.golang.org/api/urlshortener/v1"
 )
 
-func init() {
-	registerDemo("urlshortener", urlshortener.UrlshortenerScope, urlShortenerMain)
-}
-
-func urlShortenerMain(client *http.Client, argv []string) {
-	if len(argv) != 1 {
-		fmt.Fprintf(os.Stderr, "Usage: urlshortener http://goo.gl/xxxxx     (to look up details)\n")
-		fmt.Fprintf(os.Stderr, "       urlshortener http://example.com/long (to shorten)\n")
-		return
-	}
+func urlShortenerMain(client *http.Client, urls []string) {
 
 	svc, err := urlshortener.New(client)
 	if err != nil {
 		log.Fatalf("Unable to create UrlShortener service: %v", err)
 	}
-
-	urlstr := argv[0]
-
-	// short -> long
-	if strings.HasPrefix(urlstr, "http://goo.gl/") || strings.HasPrefix(urlstr, "https://goo.gl/") {
-		url, err := svc.Url.Get(urlstr).Do()
-		if err != nil {
-			log.Fatalf("URL Get: %v", err)
+	
+	for(var u := range(urls)) {
+		// short -> long
+		if strings.HasPrefix(u, "http://goo.gl/") || strings.HasPrefix(u, "https://goo.gl/") {
+			url, err := svc.Url.Get(urlstr).Do()
+			if err != nil {
+				log.Fatalf("URL Get: %v", err)
+			}
+			fmt.Printf("Lookup of %s: %s\n", u, url.LongUrl)
+			return
 		}
-		fmt.Printf("Lookup of %s: %s\n", urlstr, url.LongUrl)
-		return
+	
+		// long -> short
+		url, err := svc.Url.Insert(&urlshortener.Url{
+			Kind:    "urlshortener#url", // Not really needed
+			LongUrl: u,
+		}).Do()
+		if err != nil {
+			log.Fatalf("URL Insert: %v", err)
+		}
+		fmt.Printf("Shortened %s => %s\n", u, url.Id)
 	}
-
-	// long -> short
-	url, err := svc.Url.Insert(&urlshortener.Url{
-		Kind:    "urlshortener#url", // Not really needed
-		LongUrl: urlstr,
-	}).Do()
-	if err != nil {
-		log.Fatalf("URL Insert: %v", err)
-	}
-	fmt.Printf("Shortened %s => %s\n", urlstr, url.Id)
 }
